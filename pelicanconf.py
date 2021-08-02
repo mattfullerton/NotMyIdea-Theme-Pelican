5284407ca04880f24d16ea09a6042505eefb9b5d
F_Thoma <F_Thoma-users@codeberg.org>
2021-08-02 02:08:40 +0200

    Add an example config file

---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+SITENAME = 'Headline'
+SITESUBTITLE = 'Subtitle'
+SITEURL = ''
+ICON_URL= '/images/favicon.ico'
+
+PATH = 'content'
+THEME = '../Codeberg/NotMyIdea-Theme-Pelican/notmyidea/'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'de'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS_WIDGET_NAME = 'Links'
+LINKS = (('Impressum', 'impressum'),
+		 ('Datenschutzerklärung', 'datenschutz'),
+		 ('Design-Quellcode', '#'))
+
+# Social widget
+SOCIAL_WIDGET_NAME = 'Kontakt'
+SOCIAL = (('Email', 'mailto:email@example.org'),)
+
+STATIC_PATHS = ['images']
+
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
